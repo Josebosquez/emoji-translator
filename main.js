383fa05f47f83417ac7b4702445de39afb2eab03
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-03-28 16:21:26 -0600

    encode, translate, madlib

--- main.js
+++ main.js
@@ -1,31 +1,37 @@
 // queries
-const submitButton = document.querySelector(“#submit-button”);
-const radios = document.querySelectorAll(‘[type=“radio”]’)
-const encodeLabel = document.querySelector(“#encode”)
-const translateLabel = document.querySelector(“#translate”)
-const madlibLabel = document.querySelector(“madlib”)
+const submitButton = document.querySelector('#submit-button');
+const radios = document.querySelectorAll("[type='radio']")
+const encodeLabel = document.querySelector('#encode')
+const translateLabel = document.querySelector('#translate')
+const madlibLabel = document.querySelector('madlib')
 
 // addEventListener
-submitButton.addEventListener(“click”, function () {
-    const textInput = document.querySelector(“#translator-input”)
-    const resultH1 = document.querySelector(“#results”)
+submitButton.addEventListener('click', function () {
+    const textInput = document.querySelector('#translator-input')
+    const resultH1 = document.querySelector('#results')
     
     for (const radio of radios) {
         // if radio is selected, find out which one it is
         if (radio.checked) {
             console.log(radio.value)
-            if (radio.value === “encode”){
+            if (radio.value === "encode"){
                 let input = textInput.value
                 resultH1.innerText = encode(input)
             } 
-            if (radio.value === “translate”){
+            if (radio.value === 'translate'){
                 input = textInput.value
                 resultH1.innerText = translate(input)
             }
-            if (radio.value === “madlib”){
+            if (radio.value === 'madlib'){
+                input = textInput.value
+                resultH1.innerText = madlib(input)
+            }
+            if (radio.value === 'search'){
                 input = textInput.value
                 resultH1.innerText = madlib(input)
             }
         }
+
+        
     }
 })
