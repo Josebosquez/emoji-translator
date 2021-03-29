0af8da684d73a128ba9193eec7ffeb87aba08e9f
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-03-29 08:03:21 -0600

    failed random

--- main.js
+++ main.js
@@ -1,37 +1,94 @@
 // queries
 const submitButton = document.querySelector('#submit-button');
-const radios = document.querySelectorAll("[type='radio']")
 const encodeLabel = document.querySelector('#encode')
 const translateLabel = document.querySelector('#translate')
 const madlibLabel = document.querySelector('madlib')
+const searchedEmoji = document.querySelector("#search")
+const randomLabel = document.querySelector('#random')
+
+
 
 // addEventListener
 submitButton.addEventListener('click', function () {
     const textInput = document.querySelector('#translator-input')
-    const resultH1 = document.querySelector('#results')
-    
+    let resultH1 = document.querySelector('#results')
+    const radios = document.querySelectorAll("[type='radio']")
+    let input = textInput.value
+    let emojiObjects = search(input);
+    const rand = Math.random();
+
     for (const radio of radios) {
         // if radio is selected, find out which one it is
-        if (radio.checked) {
-            console.log(radio.value)
-            if (radio.value === "encode"){
-                let input = textInput.value
+        if (radio.checked === true) {
+    // encode radio
+            if (radio.value === "encode") {
+                console.log(radio.value)
                 resultH1.innerText = encode(input)
-            } 
-            if (radio.value === 'translate'){
-                input = textInput.value
+            }
+
+    // translate radio
+            else if (radio.value === 'translate') {
+                console.log(radio.value)
                 resultH1.innerText = translate(input)
             }
-            if (radio.value === 'madlib'){
-                input = textInput.value
+            
+    //madlib radio
+            else if (radio.value === 'madlib') {
+                console.log(radio.value)
                 resultH1.innerText = madlib(input)
             }
-            if (radio.value === 'search'){
-                input = textInput.value
-                resultH1.innerText = madlib(input)
+
+    //search radio
+            else if (radio.value === 'search') {
+                console.log(radio.value)
+                resultH1.innerHtml = ""; // create the search if statement and make it as innerHtml not innerText.
+                console.log(search(input)); // make the objects in the array display when i console log it.
+                for (elements of emojiObjects){ 
+                    console.log(elements.symbol);
+                    const newPara = document.createElement("p");
+                    newPara.innerText = elements.symbol;
+                    resultH1.appendChild(newPara);
+                }
+
+    //random radio
+            } else if (radio.value === "random"){
+                console.log(radio.value)
+                resultH1.innerHtml = math.floor(random() * 4)
+                if (resultH1.innerText === 0){
+                    // when random is hit, i want it to return the "radio" and click the submitButton.
+                }
+                if (resultH1.innerText === 1){
+                }
+                if (resultH1.innerText === 2){
+                }
+                if (resultH1.innerText === 3){
+                }    
+                
+                return resultH1
+                
             }
         }
-
         
     }
-})
+}
+)
+
+//search
+// How do i do that? 
+// i wanna make it return the emojis.symbol
+// storing that returned element.
+// using a dom element, create a paragraph to store that element. 
+// return it to resultsH1.innerHTML.
+
+
+//random
+// create or re-use a random number generator
+// use the add() function like we were taught
+
+// when random is hit, i want it to return the "radio" and submit submitButton.
+
+
+
+// function add(function("encode"), number2) {
+//     return number1 + number2;
+//   }
